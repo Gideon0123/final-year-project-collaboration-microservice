fc2057b20681fa38a8c5548dd679e8d67b1fc747
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-08-22 09:44:36 +0100

    first commit

---
+++ src/main/java/com/example/COLLABORAION_SERVICE/CollaboraionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.COLLABORAION_SERVICE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaboraionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaboraionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/COLLABORAION_SERVICE/CollaboraionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.COLLABORAION_SERVICE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaboraionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
