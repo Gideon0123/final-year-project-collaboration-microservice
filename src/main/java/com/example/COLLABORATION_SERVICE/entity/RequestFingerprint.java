6f4a0d11ea57eea71e0df3a2e1d885e1db72a313
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-09-13 01:10:23 +0100

    Idempotency Impl

---
+++ src/main/java/com/example/COLLABORATION_SERVICE/entity/RequestFingerprint.java
@@ -0,0 +1,14 @@
+package com.example.COLLABORATION_SERVICE.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class RequestFingerprint {
+
+    private Long userId;
+    private String endpoint;
+    private String httpMethod;
+    private String requestBody;
+}
